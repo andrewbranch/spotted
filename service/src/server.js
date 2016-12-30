a57fabcd891f1ff55a0cf3b4628a329397e8bc64
Andrew Branch <andrew@wheream.io>
2016-12-29 16:33:53 -0800

    Register message route with server

--- service/src/server.js
+++ service/src/server.js
@@ -1,10 +1,12 @@
 /* @flow */
 
 import Hapi from 'hapi';
+import messageRoute from './routes/message';
 
 export default () => new Promise((resolve, reject) => {
   const server = new Hapi.Server();
   server.connection({ port: process.env.PORT });
+  messageRoute(server);
   server.start(err => {
     return err ? reject(err) : resolve(server);
   });
